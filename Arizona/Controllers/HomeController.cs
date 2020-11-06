44ff1f8dc706598ded464f643f9b7d58b5163094
Oliver Sharpe <Owjsharpe@aol.com>
2020-11-06 23:22:42 +0000

    V1.0.0 Initial site with contact page added

---
+++ Arizona/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Arizona.Models;
+using Arizona.ViewModels;
+using DutchTreat.Services;
+
+namespace Arizona.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMailService _mailService;
+
+        public HomeController(ILogger<HomeController> logger, IMailService mailService)
+        {
+            _logger = logger;
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMessage("testemail@testing.com", model.Subject, $"Message from {model.Name} at {model.Email} : {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Arizona/ViewModels/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arizona.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Subject { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Message { get; set; }
+
+    }
+}
